6f88452ab96151301aebc5e4da38bce8f54207a2
AaronGu94 <68184271+AaronGu94@users.noreply.github.com>
2022-07-14 17:43:52 +0100

    Validation_loader

---
+++ PitchMap_Prediction/VRNN_loader_val.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+import os
+from glob import glob
+import warnings
+import parse
+import numpy as np
+import cv2
+from torch.utils.data import Dataset
+from numpy import *
+import pandas as pd
+
+
+class load_data_val(Dataset):
+    def __init__(self):
+        self.batch_size = 64
+        self.data = np.load('/home/aarongu/Downloads/VRNN_VAL/data.npy')
+        num_of_seq = (self.data.shape[0]//self.batch_size)*self.batch_size
+        self.data = self.data[:num_of_seq]
+        del num_of_seq
+        
+    def __getitem__(self, index):
+        x = self.data[index]
+        x = np.array(x).astype(np.float32)
+        return (x,index)
+    
+    def __len__(self):
+#        print(len(self.data))
+        return len(self.data)
